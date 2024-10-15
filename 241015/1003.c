78524182ed3ce4181286819cf71f893e61cb309f
Eunho0922 <tylereunholee010203@gmail.com>
2024-10-15 16:43:25 +0900

    241015

---
+++ 241015/1003.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main() {
+	sigset_t set;
+	int count = 5;
+
+	sigemptyset(&set);
+	sigfillset(&set);
+
+	sigprocmask(SIG_BLOCK, &set, NULL);
+
+	while(count) {
+		printf("don't disturb me (%d)\n", count--);
+		sleep(1);
+	}
+
+	sigemptyset(&set);
+	sigaddset(&set, SIGINT);
+	printf("disturb me!!\n");
+	sigprocmask(SIG_UNBLOCK, &set, NULL);
+}
